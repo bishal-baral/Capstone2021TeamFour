b592822cdc39ec2e55a5731fcd91598c229ea6b1
aliYuksekkayaBrandeis <ayuksekkaya@brandeis.edu>
2021-03-03 22:16:28 -0500

    Created databases for User and Review

---
+++ db/migrate/20210304030900_create_reviews.rb
@@ -0,0 +1,12 @@
+class CreateReviews < ActiveRecord::Migration[6.1]
+  def change
+    create_table :reviews do |t|
+      t.integer :review_id
+      t.string :content
+      t.integer :user_id
+      t.datetime :post_date
+
+      t.timestamps
+    end
+  end
+end
