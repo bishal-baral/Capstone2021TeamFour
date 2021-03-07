7dfd115bc8435c15b111e24388673c060cea0317
bishal-baral <bishalbaral@brandeis.edu>
2021-03-07 01:49:24 -0500

    created users example feed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
+  before_action :set_user, only: %i[show]
+
+  #load data from the database for the different classes (just for Hello World version, 
+  #and we are still figuring this out ;) )
   def index
+    @users = User.all
+    @reviews = Review.all
+    @friends = Friend.all
+  end
+
+  def show
+
   end
 end
