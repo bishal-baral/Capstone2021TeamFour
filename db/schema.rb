d984f20fbf142dcbb6fe53514e3ff7677db06283
aliYuksekkayaBrandeis <ayuksekkaya@brandeis.edu>
2021-03-14 22:40:28 -0400

    Created User model, updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_13_163554) do
+ActiveRecord::Schema.define(version: 2021_03_15_023349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,9 @@ ActiveRecord::Schema.define(version: 2021_03_13_163554) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.string "email"
+    t.string "name"
   end
 
 end
