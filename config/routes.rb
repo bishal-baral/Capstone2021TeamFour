159242d1c0e733e2028da69bd97f4a54c9186562
Andre Evard <andre@AndreMac.local>
2021-03-26 22:43:09 -0400

    add authentication tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
   get 'sessions/new'
-  resources :events
   get '/feed', to: 'static_pages#feed'
   get '/create_review', to: 'reviews#new'
   get '/create_event', to: 'events#new'
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,8 +1,42 @@
 require "test_helper"
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  # test "should get new" do
-  #   get sessions_new_path
-  #   assert_response :success
-  # end
+  
+  def setup
+    @pw_one = "G4qScZ5m13OvS2h7B"
+    @user_one = User.find_by(id: 216)
+    @pw_two = "2CeMh360U4kD18wBh"
+    @user_two = User.find_by(id: 217)
+  end
+  
+  test "should get new" do
+    get sessions_new_path
+    assert_response :success
+  end
+
+  test "should authenticate user" do
+    post '/login', params: { session: { email: @user_one.email, 
+                                        password: @pw_one } }
+    assert_redirected_to @user_one
+    assert session[:user_id] == 216
+  end
+
+  test "should log in and log out user" do
+    post '/login', params: { session: { email: @user_two.email, 
+                                        password: @pw_two } }
+    assert_redirected_to @user_two
+    assert_equal session[:user_id], 217
+
+    delete '/logout'
+    assert_redirected_to root_path
+    assert session[:user_id].nil?
+  end
+
+  test "should return to login page on bad credentials" do
+    post '/login', params: { session: { email: @user_two.email, 
+                                        password: @pw_one } }
+    assert_equal "Invalid email/password combo", flash[:danger]
+    assert session[:user_id].nil?
+  end
+    
 end
