223f6be1b6d9a385b9168b07aaf029b9c68eb24d
Andre Evard <andre@AndreMac.localdomain>
2021-04-09 15:00:43 -0400

    fix migrations

--- db/migrate/20210404184725_create_review_tags.rb
+++ db/migrate/20210404184725_create_review_tags.rb
@@ -3,8 +3,6 @@ class CreateReviewTags < ActiveRecord::Migration[6.1]
     create_table :review_tags do |t|
       t.belongs_to :tag
       t.belongs_to :review
-      t.integer :review_id
-      t.integer :tag_id
     end
   end
 end
