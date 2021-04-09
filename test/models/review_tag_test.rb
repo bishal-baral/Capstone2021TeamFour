f5304ada5b3c2412c5bb7be76e4ed8c7db27a1cb
Andre Evard <andre@AndreMac.localdomain>
2021-04-09 16:24:38 -0400

    add model tests

--- test/models/review_tag_test.rb
+++ test/models/review_tag_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class ReviewTagTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @rt = ReviewTag.new(review_id: 545, tag_id: 1)
+  end
+
+  test "review tag should be valid" do
+    assert @rt.valid?
+  end
+
+  test "review tag needs review" do
+    @rt.review_id = 0
+    assert_not @rt.valid?
+  end
+
+  test "review tag needs tag" do
+    @rt.tag_id = 0
+    assert_not @rt.valid?
+  end
 end
--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -1,7 +1,37 @@
 require "test_helper"
 
 class TagTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @tag = Tag.new(category: "Medium",
+                   name: "YouTube Video")
+  end
+
+  test "tag should be valid" do
+    assert @tag.valid?
+  end
+
+  test "category defaults to other" do
+    t = Tag.new(name: "Twitch streamer")
+    assert t.valid?
+  end
+
+  test "name should be required" do
+    @tag.name = nil
+    assert_not @tag.valid?
+  end
+
+  test "should be case insensitive" do
+    tag_two = Tag.create(category: "MeDiUm", name: "YOUTUBE video")
+    assert_equal tag_two.category, @tag.category
+  end
+
+  test "category length should be limited" do
+    @tag.category = "foo" * 20
+    assert_not @tag.valid?
+  end
+
+  test "name length should be limited" do
+    @tag.name = "bar" * 20
+    assert_not @tag.valid?
+  end
 end
