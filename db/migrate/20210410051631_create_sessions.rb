0288b63109efdbaa7817bb93883705fa9add57c5
bishal-baral <bishalbaral@brandeis.edu>
2021-04-11 23:40:26 -0400

    Revert "Revert "screenshare with text and video chat + invited events now show up""
    This reverts commit c97b8a91c75423291a42f3957417f62eeb9d68f6.

---
+++ db/migrate/20210410051631_create_sessions.rb
@@ -0,0 +1,9 @@
+class CreateSessions < ActiveRecord::Migration[6.1]
+  def change
+    create_table :sessions do |t|
+      t.string :session_id, null: false
+      t.boolean :expired, default: false
+      t.timestamps
+    end
+  end
+end
