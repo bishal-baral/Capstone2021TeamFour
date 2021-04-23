5be802c90dd912af44ccf1fae9c5a551f970d423
aliYuksekkayaBrandeis <ayuksekkaya@brandeis.edu>
2021-04-23 14:31:27 -0400

    added notifications model/schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_14_002305) do
+ActiveRecord::Schema.define(version: 2021_04_23_182752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,17 @@ ActiveRecord::Schema.define(version: 2021_04_14_002305) do
   create_table "invitees", force: :cascade do |t|
     t.integer "user_id"
     t.integer "event_id"
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.integer "recipient_id"
+    t.integer "actor_id"
+    t.datetime "read_at"
+    t.string "action"
+    t.integer "notifiable_id"
+    t.string "notifiable_type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "review_tags", force: :cascade do |t|
