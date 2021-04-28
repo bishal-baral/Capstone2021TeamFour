581af12f928a59d99b7051bc5cfc0d203d1a1560
Andre Evard <andre@AndreMac.localdomain>
2021-04-28 16:53:03 -0400

    fix tests, refactor session and session test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,31 +1,37 @@
-class SessionsController < ApplicationController
-  skip_before_action :require_login, only: [:new, :create]
-  before_action :require_logged_out, only: [:new, :create]
-  
-  def new
-  end
+# frozen_string_literal: true
 
+# The sessions controller is purely responsible for logging users in and out
+class SessionsController < ApplicationController
+  skip_before_action :require_login, only: %i[new create]
+  before_action :require_logged_out, only: %i[new create]
+
+  def new() end
+
+  # Log in action, needs an email and password in params
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       reset_session
-      log_in user 
-      redirect_to user 
+      log_in user
+      redirect_to user
     else
-      flash.now[:danger] = 'Invalid email/password combo'
+      flash.now[:danger] = 'Invalid email password combo'
       render 'new'
-    end 
+    end
   end
 
+  # Log out action
   def destroy
     log_out
     redirect_to root_path
   end
 
   private
-    def require_logged_out
-      if logged_in?
-        redirect_to profile_path
-      end
-    end
+
+  # Redirect logged in users away from the login page
+  def require_logged_out
+    return unless logged_in?
+
+    redirect_to profile_path
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,51 +1,53 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @username = "Dorsey"
-    @email = "jefferey@mills-anderson.info"
-    @pw = "P04gX7eAiG3fXjJ322"
+    @username = 'Dorsey'
+    @email = 'jefferey@mills-anderson.info'
+    @pw = 'P04gX7eAiG3fXjJ322'
     @other_user = User.first
   end
 
-  # test "should get new" do
-  #   get '/signup'
-  #   assert_response :success
-  # end
+  test 'should get new' do
+    get '/signup'
+    assert_response :success
+  end
 
-  # test "should be able to sign up" do
-  #   post '/signup', params: { user: { username: @username,
-  #                                     email: @email,
-  #                                     password: @pw,
-  #                                     password_confirmation: @pw } }
-  #   @user = User.find_by(username: @username)
-  #   assert_not_nil @user
-  #   assert_equal session[:user_id], @user.id
-  # end
-    
-  # test "don't sign up if passwords missmatch" do
-  #   post '/signup', params: { user: { username: @username,
-  #                                     email: @email,
-  #                                     password: "foobarbaz",
-  #                                     password_confirmation: @pw } }
-  #   assert_nil User.find_by(username: @username)
-  # end
+  test 'should be able to sign up' do
+    post '/signup', params: { user: { username: @username,
+                                      email: @email,
+                                      password: @pw,
+                                      password_confirmation: @pw } }
+    @user = User.find_by(username: @username)
+    assert_not_nil @user
+    assert_equal session[:user_id], @user.id
+  end
 
-  # test "don't sign up if email already exists" do
-  #   post '/signup', params: { user: { username: @username,
-  #                                     email: @other_user.email,
-  #                                     password: @pw,
-  #                                     password_confirmation: @pw } }
-  #   assert_nil User.find_by(username: @username)
-  # end
+  test 'don\'t sign up if passwords missmatch' do
+    post '/signup', params: { user: { username: @username,
+                                      email: @email,
+                                      password: 'foobarbaz',
+                                      password_confirmation: @pw } }
+    assert_nil User.find_by(username: @username)
+  end
 
-  # test "should show user profile" do
-  #   post '/signup', params: { user: { username: @username,
-  #                                     email: @email,
-  #                                     password: @pw,
-  #                                     password_confirmation: @pw } }
-  #   @user = User.find_by(username: @username)
-  #   get "/users/#{@user.id}"
-  #   assert_response :success
-  # end
+  test 'don\'t sign up if email already exists' do
+    post '/signup', params: { user: { username: @username,
+                                      email: @other_user.email,
+                                      password: @pw,
+                                      password_confirmation: @pw } }
+    assert_nil User.find_by(username: @username)
+  end
+
+  test 'should show user profile' do
+    post '/signup', params: { user: { username: @username,
+                                      email: @email,
+                                      password: @pw,
+                                      password_confirmation: @pw } }
+    @user = User.find_by(username: @username)
+    get "/users/#{@user.id}"
+    assert_response :success
+  end
 end
