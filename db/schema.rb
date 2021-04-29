576adb774971e6cd0b0708a9521379d594e3430a
aliYuksekkayaBrandeis <ayuksekkaya@brandeis.edu>
2021-04-29 01:04:34 -0400

    fixing the imagemagic mess-up

--- db/schema.rb
+++ db/schema.rb
@@ -62,10 +62,8 @@ ActiveRecord::Schema.define(version: 2021_04_29_014833) do
   end
 
   create_table "invitees", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "event_id"
-    t.index ["event_id"], name: "index_invitees_on_event_id"
-    t.index ["user_id"], name: "index_invitees_on_user_id"
+    t.integer "user_id"
+    t.integer "event_id"
   end
 
   create_table "notifications", force: :cascade do |t|
